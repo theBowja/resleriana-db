37d0d573991ff030e368134d02c12b29ba61463b
theBowja <ericxin@gmail.com>
2024-03-05 19:24:51 -0500

    update tool AtelierToolBundleDownload

--- import/bundle.js
+++ import/bundle.js
@@ -7,21 +7,17 @@ const config = require('./config.json');
 
 
 // const exePath = `./AtelierTool.exe download-bundles -o ./tmp ${config.fileassets_version.GL} -b ${bundleNames}`;
-const exePath = `../tools/AtelierToolCustom/AtelierTool.exe`;
-
-const args = ['download-bundles', config.fileassets_version.GL, '-o', './tmp', '-b', './images/bundlenames_android_filtered_texture2d.txt'];
+const exePath = `../tools/AtelierToolBundleDownload/AtelierTool.exe`;
+const args = [
+    'download-bundles', config.fileassets_version.GL,
+    '--platform', 'StandaloneWindows64',
+    '--server', 'Global',
+    '--output', './tmp',
+    '--bundlepath', './images/bundlenames_android_filtered_texture2d.txt'
+];
 
 console.log('downloading and decrypting bundles');
 execSync(exePath, args, { stdio: 'inherit' });
 console.log('done');
 
 
-
-
-
-
-// Handle potential errors
-// child.on('error', (err) => {
-//     console.error(`Error: ${err}`);
-// });
-  
